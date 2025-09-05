bdd03b76ad16ccba544691c1f4d7fbf68c653276
Aubin MANCEAU <aubin.manceau@wemea.fr>
2025-09-05 15:30:40 +0200

    create login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,24 +1,32 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Login() {
-  return (
-    <div className="flex flex-col lg:flex-row justify-center gap-48 items-center min-h-screen max-h-screen bg-red-400">
-        <h1 className="max-w-[18ch] text-white">Whatever happens here, <strong>stays</strong> here</h1>
-        <div className="bg-white/20 rounded-[16px] shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-[5px] border border-white/40 px-10 pb-16 pt-8 h-[400px] max-lg:max-w-[300px]">
-            <form className="flex flex-col justify-between h-full">
-                <h2 className="text-white">Se connecter</h2>
-                <div className="flex flex-col gap-4">
-                    <div className="flex flex-col relative">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id="email"/>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <div className="flex flex-col lg:flex-row justify-center gap-12 items-center min-h-screen px-4 bg-[url('../public/bg-login.jpg')] bg-cover bg-center relative">
+            <div className="absolute top-0 left-0 bg-bleu/85 w-full h-full"></div>
+            <h1 className="lg:max-w-[18ch] text-white z-1">Accès réservé, admins de l'<span>ESQ</span> seulement.</h1>
+            <div className="bg-white/20 rounded-[16px] shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-[5px] border border-white/40 px-4 lg:px-10 py-6 lg:py-16 lg:h-[500px] h-[350px] w-full max-w-[600px] z-1">
+                <form className="flex flex-col justify-between h-full">
+                    <h2 className="text-white">Se connecter</h2>
+                    <div className="flex flex-col gap-4">
+                        <div className="flex flex-col relative">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} onFocus={(e) => e.target.parentNode.classList.add("focused")} onBlur={(e) => {if (!e.target.value) e.target.parentNode.classList.remove("focused")}} />
+                        </div>
+                        <div className="flex flex-col relative">
+                            <label htmlFor="password">Mot de passe</label>
+                            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} onFocus={(e) => e.target.parentNode.classList.add("focused")} onBlur={(e) => {if (!e.target.value) e.target.parentNode.classList.remove("focused")}} />
+                        </div>
+                        <a href="#" className="forgot-password">Mot de passe oublié ?</a>
                     </div>
-                    <div className="flex flex-col relative">
-                        <label htmlFor="password">Mot de passe</label>
-                        <input type="password" id="password"/>
-                    </div>
-                    <a href="#" className="flex justify-end text-white">Mot de passe oublié ?</a>
-                </div>
-                <button type="submit" className="btn">Se connecter</button>
-            </form>
+                    <button type="submit" className="btn">Se connecter</button>
+                </form>
+            </div>
         </div>
-    </div>
-  );
+    );
 }

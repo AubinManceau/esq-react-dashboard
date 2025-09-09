f6ce0249a3f77c43c4efbf1657e61731e3e25c3e
Aubin MANCEAU <aubin.manceau@wemea.fr>
2025-09-09 14:52:57 +0200

    update dashboard

--- app/(pages)/layout.jsx
+++ app/(pages)/layout.jsx
@@ -5,10 +5,6 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
       <body>
         {children}
       </body>
--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { Menu, X, LogOut, House, Users, Newspaper, Scroll, Shirt } from "lucide-react";
 
 export default function Sidebar() {
@@ -8,7 +9,7 @@ export default function Sidebar() {
 
   return (
     <>
-      <button onClick={() => setOpen(true)} className="lg:hidden fixed top-4 left-4 z-50 p-2 bg-gray-800 text-white rounded-md">
+      <button onClick={() => setOpen(true)} className="lg:hidden fixed top-4 left-4 z-50 p-2 bg-orange text-white rounded-md">
         <Menu size={24} />
       </button>
 
@@ -16,75 +17,77 @@ export default function Sidebar() {
         <div className="fixed inset-0 bg-black opacity-50 z-40 lg:hidden" onClick={() => setOpen(false)} />
       )}
 
-      <aside className={`fixed top-0 left-0 h-screen w-64 bg-white py-8 px-2 flex flex-col justify-between transform transition-transform duration-300 z-50 ${open ? "translate-x-0" : "-translate-x-full"} lg:translate-x-0 lg:static`}>
+      <aside className={`fixed top-0 left-0 h-screen w-64 bg-white flex flex-col justify-between transform transition-transform duration-300 z-50 ${open ? "translate-x-0" : "-translate-x-full"} lg:translate-x-0 lg:static`}>
         <button onClick={() => setOpen(false)} className="lg:hidden absolute top-4 right-4">
-          <X size={24} />
+          <X size={24} color="white" />
         </button>
         <nav>
-          <h2 className="text-2xl font-bold !mb-8 text-center">ESQ</h2>
-          <ul className="space-y-4">
-            <li>
+          <div className="bg-orange py-8 mb-10">
+            <h2 className="!mb-0 pl-6 !font-default-extralight text-white">Etoile Sportive <span className="font-default-medium">Quelainaise</span></h2>
+          </div>
+          <div className="flex flex-col gap-4 px-3">
+            <div>
               <Link
                 href="/admin"
-                className="flex gap-2 items-center py-2 px-2 rounded hover:bg-gray-100"
+                className="nav-link"
                 onClick={() => setOpen(false)}
               >
-                <House />
+                <House color="#FC6E11" />
                 Tableau de bord
               </Link>
-            </li>
-            <li>
+            </div>
+            <div>
               <Link
                 href="/admin/utilisateurs"
-                className="flex gap-2 items-center py-2 px-2 rounded hover:bg-gray-100"
+                className="nav-link"
                 onClick={() => setOpen(false)}
               >
-                <Users />
+                <Users color="#FC6E11" />
                 Utilisateurs
               </Link>
-            </li>
-            <li>
+            </div>
+            <div>
               <Link
                 href="/admin/articles"
-                className="flex gap-2 items-center py-2 px-2 rounded hover:bg-gray-100"
+                className="nav-link"
                 onClick={() => setOpen(false)}
               >
-                <Newspaper />
+                <Newspaper color="#FC6E11" />
                 Articles
               </Link>
-            </li>
-            <li>
+            </div>
+            <div>
               <Link
                 href="/admin/equipes"
-                className="flex gap-2 items-center py-2 px-2 rounded hover:bg-gray-100"
+                className="nav-link"
                 onClick={() => setOpen(false)}
               >
-                <Shirt />
+                <Shirt color="#FC6E11" />
                 Équipes
               </Link>
-            </li>
-            <li>
+            </div>
+            <div>
               <Link
                 href="/admin/convocations"
-                className="flex gap-2 items-center py-2 px-2 rounded hover:bg-gray-100"
+                className="nav-link"
                 onClick={() => setOpen(false)}
               >
-                <Scroll />
+                <Scroll color="#FC6E11" />
                 Convocations
               </Link>
-            </li>
-          </ul>
+            </div>
+          </div>
         </nav>
 
-        <div>
-          <button className="flex items-center gap-6 bg-gray-100 rounded-md py-2 px-6 w-full cursor-pointer">
-            <LogOut />
-            <div className="flex flex-col text-left">
-              <p>Aubin Manceau</p>
-              <span className="text-sm">Se déconnecter</span>
-            </div>
-          </button>
-        </div>
+        <button className="flex items-center gap-6 rounded-md mb-8 py-3 px-6 cursor-pointer">
+          <div className="bg-orange p-3 rounded-full">
+            <LogOut color="white" />
+          </div>
+          <div className="flex flex-col text-left">
+            <p className="h4">Aubin Manceau</p>
+            <span className="logout">Se déconnecter</span>
+          </div>
+        </button>
       </aside>
     </>
   );

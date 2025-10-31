48b44cabbf2929adaf61e5578e8220f4db989444
Aubin MANCEAU <aubin.manceau@wemea.fr>
2025-10-31 17:01:20 +0100

    update

--- app/admin/utilisateurs/inscription/page.jsx
+++ app/admin/utilisateurs/inscription/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import UserForm from "@/components/UserForm";
 import BulkUserForm from "@/components/BulkUserForm";
 import { useProtectedRoute } from "@/contexts/useProtectedRoute";
